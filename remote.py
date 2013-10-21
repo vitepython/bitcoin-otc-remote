211a07e37d86d4a04eb1e7f776a4d8341c3ba672
Pensando Panama <vitepython@users.noreply.github.com>
2013-10-20 22:33:44 -0500

    Update remote.py

--- remote.py
+++ remote.py
@@ -4,6 +4,26 @@ import socket
 import httplib
 import gnupg
 
+print '''
+bitcoin otc remote version 0.01
+Pensando Panama Copyright 2013
+
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+'''
+
 # This is to identify your user/pass to the IRC server so your bot can be masked
 # Thought it was safer to have it on user input rather than hardcoded            
 user = raw_input("enter user ")
